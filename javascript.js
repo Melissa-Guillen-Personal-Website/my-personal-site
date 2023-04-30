e200ea9b6b8a44befaeb603a6703fc1e04265b52
Melissa Guillen <melissaguillen70@yahoo.com>
2023-04-30 01:25:17 -0700

    Final changes before submitting assignment

--- javascript.js
+++ javascript.js
@@ -1,10 +1,10 @@
 console.log("Hello World! Welcome to my portfolio.")
 
 const form = document.querySelector("#form")
-   const submitButton = document.querySelector("#submit")
-   const scriptURL = 'https://script.google.com/macros/s/AKfycbx7c12RzXX85c96-OO5xiOKF2A__c61QDSXxHIiiN0/dev'
+const submitButton = document.querySelector("#submit")
+const scriptURL = 'https://script.google.com/macros/s/AKfycby7ogGoNAinUdGEnCuToqavXSDfEa0jrJekIoRSTkDTEUYxrkTBUG4D1rap0yJmGHT2/exec'
 
-   form.addEventListener('submit', e => {
+  form.addEventListener('submit', e => {
      submitButton.disabled = true
      e.preventDefault()
      let requestBody = new FormData(form)
@@ -19,4 +19,4 @@ const form = document.querySelector("#form")
 
        }
        )
-   })
+  })
