699aa0bbb7574982db50a96b0561ec607bbcc8df
Melissa Guillen <melissaguillen70@yahoo.com>
2023-04-29 23:23:42 -0700

    updates to files almost ready to submit assignment

--- javascript.js
+++ javascript.js
@@ -1,1 +1,22 @@
 console.log("Hello World! Welcome to my portfolio.")
+
+const form = document.querySelector("#form")
+   const submitButton = document.querySelector("#submit")
+   const scriptURL = 'https://script.google.com/macros/s/AKfycbx7c12RzXX85c96-OO5xiOKF2A__c61QDSXxHIiiN0/dev'
+
+   form.addEventListener('submit', e => {
+     submitButton.disabled = true
+     e.preventDefault()
+     let requestBody = new FormData(form)
+     fetch(scriptURL, { method: 'POST', body: requestBody})
+       .then(response => {
+          alert('Success!', response)
+          submitButton.disabled = false
+         })
+       .catch(error => {
+       alert('Error!', error.message)
+         submitButton.disabled = false
+
+       }
+       )
+   })
